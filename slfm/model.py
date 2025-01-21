19044b77bad8721c36aa02e8accde6447ab13f0d
VigneshSrinivasan10 <vignesh.mssrinivasan@gmail.com>
2025-01-21 13:54:16 +0100

    trimmed some parts of previous mup implementation

--- slfm/model.py
+++ slfm/model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from mup import MuReadout
 from collections import defaultdict
 
 class FlowModel(nn.Module):
@@ -42,7 +41,6 @@ class FlowMLP(FlowModel):
                 nn.SiLU(),
             ))
         self.blocks = nn.ModuleList(blocks)
-        #self.final = MuReadout(width, n_features, bias=False, output_mult=self.output_mult) #
         self.final = nn.Linear(width, n_features, bias=False)
         self.reset_parameters()
 
@@ -110,37 +108,3 @@ class FlowMLP(FlowModel):
 
         return optimizer, final_optimizer_settings
 
-
-class FlowNeuralNetwork(FlowModel):
-
-    def __init__(self, n_features, width=10, nonlin=F.relu, output_mult=1.0, input_mult=1.0):
-        super().__init__()
-        self.time_embedding_size = width - n_features
-        self.time_embedding = ZeroToOneTimeEmbedding(self.time_embedding_size)
-        
-        self.nonlin = nonlin
-        self.input_mult = input_mult
-        self.output_mult = output_mult
-
-
-        self.fc_1 = nn.Linear(width, width, bias=False)
-        self.fc_2 = nn.Linear(width, width, bias=False)
-        self.final = MuReadout(width, n_features, bias=False, output_mult=self.output_mult)
-        #self.final = nn.Linear(width, n_features, bias=False)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        nn.init.kaiming_normal_(self.fc_1.weight, a=1, mode='fan_in')
-        self.fc_1.weight.data /= self.input_mult**0.5
-        nn.init.kaiming_normal_(self.fc_2.weight, a=1, mode='fan_in')
-        nn.init.kaiming_normal_(self.final.weight, a=1, mode='fan_in')
-        #nn.init.zeros_(self.final.weight)
-
-
-    def forward(self, X, time=None):
-        if time is None:
-            time = torch.rand(X.shape[0], device=X.device)
-        X = torch.cat([X, self.time_embedding(time)], axis=1)
-        out = self.nonlin(self.fc_1(X) * self.input_mult**0.5)
-        out = self.nonlin(self.fc_2(out))
-        return self.final(out)  #* self.output_mult
